fc01209da22015e0999abe29dbd1252295e761bb
benfordi25 <ben_ford123@hotmail.co.uk>
2026-01-05 11:55:28 +0000

    Update sw.js

--- works-completion-report/works-completion-report/works-completion-report/sw.js
+++ works-completion-report/works-completion-report/works-completion-report/sw.js
@@ -1,1 +1,30 @@
+const CACHE_NAME = "fps-works-completion-report-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "https://cdn.jsdelivr.net/npm/jspdf@2.5.1/dist/jspdf.umd.min.js"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
